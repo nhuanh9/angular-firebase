02910657adccd257a39dc3299e258e65771bec60
nhuanh9 <chopperlouis@gmail.com>
2022-07-08 00:09:00 +0700

    done reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,5 @@ import {finalize} from "rxjs/operators";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "cloudsSorage";
-  selectedFile: any;
-  fb: any;
-  downloadURL: any;
 
-  constructor(private storage: AngularFireStorage) {
-  }
-
-  ngOnInit() {
-  }
-
-  onFileSelected(event: any) {
-    var n = Date.now();
-    const file = event.target.files[0];
-    const filePath = `RoomsImages/${n}`;
-    const fileRef = this.storage.ref(filePath);
-    const task = this.storage.upload(`RoomsImages/${n}`, file);
-    task
-      .snapshotChanges()
-      .pipe(
-        finalize(() => {
-          this.downloadURL = fileRef.getDownloadURL();
-          this.downloadURL.subscribe((url:any) => {
-            if (url) {
-              this.fb = url;
-            }
-            console.log(this.fb);
-          });
-        })
-      )
-      .subscribe(url => {
-        if (url) {
-          console.log(url);
-        }
-      });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AngularFireStorageModule} from "@angular/fire/compat/storage";
-import {environment} from "../environments/environment";
-import {AngularFireModule} from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
@@ -14,8 +11,6 @@ import {AngularFireModule} from "@angular/fire/compat";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireStorageModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig, "cloud")
   ],
   providers: [],
   bootstrap: [AppComponent]
