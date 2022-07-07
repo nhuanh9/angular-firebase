b29a9bc0f21e44e1b561a3b321bf1fbac8148b59
nhuanh9 <chopperlouis@gmail.com>
2022-07-08 01:55:56 +0700

    done test jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LoginComponent } from './component/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthenticationService} from "../../service/authentication.service";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.authenticationService.login('linhcau', '123456')
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+          localStorage.setItem('ACCESS_TOKEN', data.accessToken);
+          localStorage.setItem('ROLE', data.roles[0].authority);
+          localStorage.setItem('USERNAME', data.username);
+
+        },
+        error => {
+          alert("Tài khoản của bạn sai mật khẩu!");
+        });
   }
 
 }
